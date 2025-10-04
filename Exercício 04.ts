d958c7fcfed4601ac1e309ec6d9dea1760cf42b3
Samarah Mustafá <samarah.dev@gmail.com>
2025-10-04 17:32:46 -0300

    Nome e Sobrenome

--- Exercício 04.ts
+++ Exercício 04.ts
@@ -1,7 +1,7 @@
 //Exercício 4: Declare duas variáveis chamadas "nome" e "sobrenome", atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira variável chamada "nomeCompleto" e imprima.
 
-let nome: string = "Samarah";
-let sobrenome: string = "Mustafá";
-let nomeCompleto: string = nome + " " + sobrenome;
+const nome: string = "Samarah";
+const sobrenome: string = "Mustafá";
+const nomeCompleto: string = nome + " " + sobrenome;
 
 console.log(nomeCompleto);
