3e32ffb90e17613af2b4ed41103d9b3331521d52
Samarah Mustafá <samarah.dev@gmail.com>
2025-10-22 18:47:44 -0300

    Utilizando operador de incremento

---
+++ Exercício 008.ts
@@ -0,0 +1,7 @@
+//Exercício 8: Declare uma variável e atribua um valor numérico a ela.
+//Use o operador de incremento para aumentar o valor da variável.
+//Imprima o resultado.
+
+let num: number = 25;
+num++;
+console.log(num);
