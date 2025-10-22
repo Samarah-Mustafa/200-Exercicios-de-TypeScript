e7815f139693928e3a34cc0a15343636d923070c
Samarah Mustafá <samarah.dev@gmail.com>
2025-10-22 18:20:42 -0300

    Adição entre dois números

---
+++ Exercício 005.ts
@@ -0,0 +1,9 @@
+//Exercício 5: Declare duas variáveis e atribua valores numéricos a elas.
+// Use o operador de adição para realizar operações entre as duas variáveis.
+// Imprima o resultado.
+
+let num1: number = 30;
+let num2: number = 15;
+let resultado: number = num1 + num2;
+
+console.log("O resultado da adição entre " + num1 + " e " + num2 + " é: " + resultado);
