37395acb81d99a55543218567eddc14a20f4bdf4
Samarah Mustafá <samarah.dev@gmail.com>
2025-11-11 22:06:28 -0300

    Imprimindo todos os números perfeitos de 1 a 100

---
+++ Exercício 036.ts
@@ -0,0 +1,14 @@
+/*Exercício 36: Usando um laço for, imprima todos os números perfeitos de 1 a 100
+(um número é perfeito se a soma de seus divisores, incluindo 1 mas não ele mesmo,
+é igual ao próprio número).*/
+for (let num: number = 1; num <= 100; num++) {
+    let somaDivisores: number = 0;
+    for (let i: number = 1; i < num; i++) {
+        if (num % i === 0) {
+            somaDivisores += i;
+        }
+    }
+    if (somaDivisores === num) {
+        console.log(num);
+    }
+}
