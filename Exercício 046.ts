787a0346f15d2e568c7ad7343c7ba0947ac89377
Samarah Mustafá <samarah.dev@gmail.com>
2025-11-16 13:49:07 -0300

    Função que retorne a soma de dois números

---
+++ Exercício 046.ts
@@ -0,0 +1,5 @@
+//Exercício 46: Escreva uma função que aceite dois números como argumentos e retorne sua soma.
+function soma(a: number, b: number): number {
+    return a + b;
+}
+console.log(soma(3, 5));
