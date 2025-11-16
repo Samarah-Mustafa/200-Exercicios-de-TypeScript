ef629a8825129bd39277ff618a4c56afe706b837
Samarah Mustafá <samarah.dev@gmail.com>
2025-11-16 14:03:24 -0300

    Função que retorne o quadrado de um número

---
+++ Exercício 047.ts
@@ -0,0 +1,5 @@
+//Exercício 47: Escreva uma função que aceite um número como argumento e retorne o seu quadrado.
+function quadrado(num: number): number {
+    return num * num;
+}
+console.log(quadrado(4));
